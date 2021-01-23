bf3af3891bfbbd81615aac1867175dcb6c298145
kenjis <kenji.uui@gmail.com>
2021-01-23 10:44:26 +0900

    style: composer cs-fix

--- app/Controllers/Form.php
+++ app/Controllers/Form.php
@@ -1,6 +1,9 @@
 <?php
+
+declare(strict_types=1);
+
 /* コンタクトフォーム
- * 
+ *
  */
 
 /**
@@ -10,136 +13,126 @@
  * @property CI_Input $input
  * @property CI_Output $output
  */
-class Form extends CI_Controller {
+class Form extends CI_Controller
+{
+    public function __construct()
+    {
+// 親クラスのコンストラクタを呼び出します。コントローラにコンストラクタを
+// 記述する場合は、忘れずに記述してください。
+        parent::__construct();
 
-	public function __construct()
-	{
-# 親クラスのコンストラクタを呼び出します。コントローラにコンストラクタを
-# 記述する場合は、忘れずに記述してください。
-		parent::__construct();
+// 必要なヘルパーをロードします。
+        $this->load->helper(['form', 'url']);
 
-# 必要なヘルパーをロードします。
-		$this->load->helper(['form', 'url']);
-		
-# セッションクラスをロードすることで、セッションを開始します。
-		$this->load->library('session');
+// セッションクラスをロードすることで、セッションを開始します。
+        $this->load->library('session');
 
-# 出力クラスのset_header()メソッドでHTTPヘッダのContent-Typeヘッダを指定
-# します。
-		$this->output->set_header('Content-Type: text/html; charset=UTF-8');
+// 出力クラスのset_header()メソッドでHTTPヘッダのContent-Typeヘッダを指定
+// します。
+        $this->output->set_header('Content-Type: text/html; charset=UTF-8');
 
-# バリデーション(検証)クラスをロードします。
-		$this->load->library('form_validation');
+// バリデーション(検証)クラスをロードします。
+        $this->load->library('form_validation');
 
-		//$this->output->enable_profiler(TRUE);
-	}
+        //$this->output->enable_profiler(TRUE);
+    }
 
-	private function _set_validation()
-	{
-# バリデーションの設定をします。
-		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		$this->form_validation->set_rules('name', '名前', 'trim|required|max_length[20]');
-		$this->form_validation->set_rules('email', 'メールアドレス', 'trim|required|valid_email');
-		$this->form_validation->set_rules('comment', 'コメント', 'required|max_length[200]');
-	}
+    private function _set_validation(): void
+    {
+// バリデーションの設定をします。
+        $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+        $this->form_validation->set_rules('name', '名前', 'trim|required|max_length[20]');
+        $this->form_validation->set_rules('email', 'メールアドレス', 'trim|required|valid_email');
+        $this->form_validation->set_rules('comment', 'コメント', 'required|max_length[200]');
+    }
 
-	public function index()
-	{
-# 入力ページ(form)のビューをロードし表示します。
-		$this->load->view('form');
-	}
+    public function index(): void
+    {
+// 入力ページ(form)のビューをロードし表示します。
+        $this->load->view('form');
+    }
 
-	public function confirm()
-	{
-# 検証ルールを設定します。
-		$this->_set_validation();
+    public function confirm(): void
+    {
+// 検証ルールを設定します。
+        $this->_set_validation();
 
-# バリデーション(検証)クラスのrun()メソッドを呼び出し、送信されたデータの検証
-# を行います。検証OKなら、確認ページ(form_confirm)を表示します。
-		if ($this->form_validation->run() == TRUE)
-		{
-			$this->load->view('form_confirm');
-		}
-# 検証でエラーの場合、入力ページ(form)を表示します。
-		else
-		{
-			$this->load->view('form');
-		}
-	}
+// バリデーション(検証)クラスのrun()メソッドを呼び出し、送信されたデータの検証
+// を行います。検証OKなら、確認ページ(form_confirm)を表示します。
+        if ($this->form_validation->run() == true) {
+            $this->load->view('form_confirm');
+        }
+// 検証でエラーの場合、入力ページ(form)を表示します。
+        else {
+            $this->load->view('form');
+        }
+    }
 
-	public function send()
-	{
-# 検証ルールを設定します。
-		$this->_set_validation();
+    public function send(): void
+    {
+// 検証ルールを設定します。
+        $this->_set_validation();
 
-# 送信されたデータの検証を行い、検証OKなら、メールを送信します。
-		if ($this->form_validation->run() == TRUE)
-		{
-# メールの内容を設定します。
-			$mail = [];
-			$mail['from_name'] = $this->input->post('name');
-			$mail['from']      = $this->input->post('email');
-			$mail['to']        = 'info@example.jp';
-			$mail['subject']   = 'コンタクトフォーム';
-			$mail['body']      = $this->input->post('comment');
+// 送信されたデータの検証を行い、検証OKなら、メールを送信します。
+        if ($this->form_validation->run() == true) {
+// メールの内容を設定します。
+            $mail = [];
+            $mail['from_name'] = $this->input->post('name');
+            $mail['from']      = $this->input->post('email');
+            $mail['to']        = 'info@example.jp';
+            $mail['subject']   = 'コンタクトフォーム';
+            $mail['body']      = $this->input->post('comment');
 
-# _sendmail()メソッドを呼び出しメールの送信処理を行います。
-# メールの送信に成功したら、完了ページ(form_end)を表示します。
-			if ($this->_sendmail($mail))
-			{
-# 完了ページ(form_end)を表示し、セッションを破棄します。
-				$this->load->view('form_end');
-				$this->session->sess_destroy();
-			}
-# メールの送信に失敗した場合、エラーを表示します。
-			else
-			{
-				echo 'メール送信エラー';
-			}
-		}
-# 検証でエラーの場合、入力ページ(form)を表示します。
-		else
-		{
-			$this->load->view('form');
-		}
-	}
+// _sendmail()メソッドを呼び出しメールの送信処理を行います。
+// メールの送信に成功したら、完了ページ(form_end)を表示します。
+            if ($this->_sendmail($mail)) {
+// 完了ページ(form_end)を表示し、セッションを破棄します。
+                $this->load->view('form_end');
+                $this->session->sess_destroy();
+            }
+// メールの送信に失敗した場合、エラーを表示します。
+            else {
+                echo 'メール送信エラー';
+            }
+        }
+// 検証でエラーの場合、入力ページ(form)を表示します。
+        else {
+            $this->load->view('form');
+        }
+    }
 
-	private function _sendmail($mail)
-	{
-# Emailクラスをロードします。
-		$this->load->library('email');
-		$config = [];
-# メールの送信方法を指定します。ここでは、mail()関数を使います。
-		$config['protocol'] = 'mail';
-# 日本語ではワードラップ機能は使えませんのでオフにします。
-		$config['wordwrap'] = FALSE;
-# $configでEmailクラスを初期化します。
-		$this->email->initialize($config);
+    private function _sendmail($mail)
+    {
+// Emailクラスをロードします。
+        $this->load->library('email');
+        $config = [];
+// メールの送信方法を指定します。ここでは、mail()関数を使います。
+        $config['protocol'] = 'mail';
+// 日本語ではワードラップ機能は使えませんのでオフにします。
+        $config['wordwrap'] = false;
+// $configでEmailクラスを初期化します。
+        $this->email->initialize($config);
 
-# メールの内容を変数に代入します。
-		$from_name = $mail['from_name'];
-		$from      = $mail['from'];
-		$to        = $mail['to'];
-		$subject   = $mail['subject'];
-		$body      = $mail['body'];
+// メールの内容を変数に代入します。
+        $from_name = $mail['from_name'];
+        $from      = $mail['from'];
+        $to        = $mail['to'];
+        $subject   = $mail['subject'];
+        $body      = $mail['body'];
 
-# 差出人、あて先、件名、本文をEmailクラスに設定します。
-		$this->email->from($from, $from_name);
-		$this->email->to($to);
-		$this->email->subject($subject);
-		$this->email->message($body);
+// 差出人、あて先、件名、本文をEmailクラスに設定します。
+        $this->email->from($from, $from_name);
+        $this->email->to($to);
+        $this->email->subject($subject);
+        $this->email->message($body);
 
-# Emailクラスのsend()メソッドで、実際にメールを送信します。
-# メールの送信が成功した場合はTRUEを、失敗した場合はFALSEを返します。
-		if ($this->email->send())
-		{
-			return TRUE;
-		}
-		else
-		{
-//			echo $this->email->print_debugger();
-			return FALSE;
-		}
-	}
+// Emailクラスのsend()メソッドで、実際にメールを送信します。
+// メールの送信が成功した場合はTRUEを、失敗した場合はFALSEを返します。
+        if ($this->email->send()) {
+            return true;
+        }
 
+        //          echo $this->email->print_debugger();
+        return false;
+    }
 }
--- app/Controllers/Shop.php
+++ app/Controllers/Shop.php
@@ -1,6 +1,9 @@
 <?php
+
+declare(strict_types=1);
+
 /* 簡易ショッピングカート
- * 
+ *
  */
 
 /**
@@ -16,245 +19,249 @@
  * @property CI_Config $config
  * @property CI_Input $input
  */
-class Shop extends MY_Controller {
+class Shop extends MY_Controller
+{
+    public $limit;  // 1ページに表示する商品の数
+    public $admin;  // 管理者のメールアドレス
 
-	public $limit;	// 1ページに表示する商品の数
-	public $admin;	// 管理者のメールアドレス
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['session', 'twig']);
+        $this->load->helper(['form', 'url']);
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->library(['session', 'twig']);
-		$this->load->helper(['form', 'url']);
+// モデルをロードします。ロード後のモデルオブジェクトは、$this->shop_modelなど
+// として利用できます。
+        $this->load->model([
+            'shop/shop_model',
+            'shop/inventory_model',
+            'shop/cart_model',
+            'shop/customer_model',
+        ]);
 
-# モデルをロードします。ロード後のモデルオブジェクトは、$this->shop_modelなど
-# として利用できます。
-		$this->load->model([
-			'shop/shop_model',
-			'shop/inventory_model',
-			'shop/cart_model',
-			'shop/customer_model',
-		]);
+// このアプリケーション専用の設定ファイルconfig_shop.phpを読み込みます。
+// load()メソッドの第2引数にTRUEを指定すると、他の設定ファイルで使われている
+// 設定項目名との衝突を気にしなくても済みます。
+        $this->config->load('config_shop', true);
+// 上記のように読み込んだ場合、設定値は、以下のようにitem()メソッドに引数で
+// 「設定項目名」と「設定ファイル名」を渡すことで取得できます。
+        $this->limit = $this->config->item('per_page', 'config_shop');
+        $this->admin = $this->config->item('admin_email', 'config_shop');
+    }
 
-# このアプリケーション専用の設定ファイルconfig_shop.phpを読み込みます。
-# load()メソッドの第2引数にTRUEを指定すると、他の設定ファイルで使われている
-# 設定項目名との衝突を気にしなくても済みます。
-		$this->config->load('config_shop', TRUE);
-# 上記のように読み込んだ場合、設定値は、以下のようにitem()メソッドに引数で
-# 「設定項目名」と「設定ファイル名」を渡すことで取得できます。
-		$this->limit = $this->config->item('per_page', 'config_shop');
-		$this->admin = $this->config->item('admin_email', 'config_shop');
-	}
+    // トップページ = カテゴリ別商品一覧
+    public function index($cat_id = '1', $offset = '0'): void
+    {
+// カテゴリーIDとオフセットを検証します。
+        $this->load->library('validation/field_validation');
+        $this->field_validation->validate(
+            $cat_id,
+            'required|is_natural|max_length[11]'
+        );
+        $this->field_validation->validate(
+            $offset,
+            'required|is_natural|max_length[3]'
+        );
 
-	// トップページ = カテゴリ別商品一覧
-	public function index($cat_id = '1', $offset = '0')
-	{
-# カテゴリーIDとオフセットを検証します。
-		$this->load->library('validation/field_validation');
-		$this->field_validation->validate(
-			$cat_id, 'required|is_natural|max_length[11]'
-		);
-		$this->field_validation->validate(
-			$offset, 'required|is_natural|max_length[3]'
-		);
+        $data = [];
 
-		$data = [];
+// モデルからカテゴリの一覧を取得します。
+        $data['cat_list'] = $this->inventory_model->get_category_list();
 
-# モデルからカテゴリの一覧を取得します。
-		$data['cat_list'] = $this->inventory_model->get_category_list();
+// カテゴリIDとoffset値と、1ページに表示する商品の数を渡し、モデルより
+// 商品一覧を取得します。
+        $data['list'] = $this->inventory_model->get_product_list(
+            $cat_id,
+            $this->limit,
+            $offset
+        );
+// カテゴリIDより、カテゴリ名を取得します。
+        $data['category'] = $this->inventory_model->get_category_name($cat_id);
 
-# カテゴリIDとoffset値と、1ページに表示する商品の数を渡し、モデルより
-# 商品一覧を取得します。
-		$data['list'] = $this->inventory_model->get_product_list(
-			$cat_id, $this->limit, $offset
-		);
-# カテゴリIDより、カテゴリ名を取得します。
-		$data['category'] = $this->inventory_model->get_category_name($cat_id);
+// モデルよりそのカテゴリの商品数を取得し、ページネーションを生成します。
+        $this->load->library('generate_pagination');
+        $path  = '/shop/index/' . $cat_id;
+        $total = $this->inventory_model->get_product_count($cat_id);
+        $data['pagination'] = $this->generate_pagination->get_links($path, $total, 4);
 
-# モデルよりそのカテゴリの商品数を取得し、ページネーションを生成します。
-		$this->load->library('generate_pagination');
-		$path  = '/shop/index/' . $cat_id;
-		$total = $this->inventory_model->get_product_count($cat_id);
-		$data['pagination'] = $this->generate_pagination->get_links($path, $total, 4);
+        $data['total'] = $total;
 
-		$data['total'] = $total;
+        $data['main'] = 'shop_list';
 
-		$data['main'] = 'shop_list';
+// モデルよりカートの中の商品アイテム数を取得します。
+        $data['item_count'] = $this->cart_model->count();
 
-# モデルよりカートの中の商品アイテム数を取得します。
-		$data['item_count'] = $this->cart_model->count();
+// ビューを表示します。
+        $this->twig->display('shop_tmpl_shop', $data);
+    }
 
-# ビューを表示します。
-		$this->twig->display('shop_tmpl_shop', $data);
-	}
+    // 商品詳細ページ
+    public function product($prod_id = '1'): void
+    {
+// 商品IDを検証します。
+        $this->load->library('validation/field_validation');
+        $this->field_validation->validate(
+            $prod_id,
+            'required|is_natural|max_length[11]'
+        );
 
-	// 商品詳細ページ
-	public function product($prod_id = '1')
-	{
-# 商品IDを検証します。
-		$this->load->library('validation/field_validation');
-		$this->field_validation->validate(
-			$prod_id, 'required|is_natural|max_length[11]'
-		);
+        $data = [];
+        $data['cat_list'] = $this->inventory_model->get_category_list();
 
-		$data = [];
-		$data['cat_list'] = $this->inventory_model->get_category_list();
+// モデルより商品データを取得します。
+        $data['item'] = $this->inventory_model->get_product_item($prod_id);
+        $data['main'] = 'shop_product';
 
-# モデルより商品データを取得します。
-		$data['item'] = $this->inventory_model->get_product_item($prod_id);
-		$data['main'] = 'shop_product';
+        $data['item_count'] = $this->cart_model->count();
+        $this->twig->display('shop_tmpl_shop', $data);
+    }
 
-		$data['item_count'] = $this->cart_model->count();
-		$this->twig->display('shop_tmpl_shop', $data);
-	}
+    // カゴに入れる
+    public function add($prod_id = '0'): void
+    {
+// 商品IDを検証します。
+        $this->load->library('validation/field_validation');
+        $this->field_validation->validate(
+            $prod_id,
+            'required|is_natural|max_length[11]'
+        );
+// POSTされたqtyフィールドより、数量を取得します。
+        $qty = $this->input->post('qty');
+// 数量を検証します。
+        $this->field_validation->validate(
+            $qty,
+            'required|is_natural|max_length[3]'
+        );
 
-	// カゴに入れる
-	public function add($prod_id = '0')
-	{
-# 商品IDを検証します。
-		$this->load->library('validation/field_validation');
-		$this->field_validation->validate(
-			$prod_id, 'required|is_natural|max_length[11]'
-		);
-# POSTされたqtyフィールドより、数量を取得します。
-		$qty = $this->input->post('qty');
-# 数量を検証します。
-		$this->field_validation->validate(
-			$qty, 'required|is_natural|max_length[3]'
-		);
+        $this->cart_model->add($prod_id, $qty);
 
-		$this->cart_model->add($prod_id, $qty);
+// コントローラのcart()メソッドを呼び出し、カートを表示します。
+        $this->cart();
+    }
 
-# コントローラのcart()メソッドを呼び出し、カートを表示します。
-		$this->cart();
-	}
+    // 買い物カゴページ
+    public function cart(): void
+    {
+        $data = [];
+        $data['cat_list'] = $this->inventory_model->get_category_list();
 
-	// 買い物カゴページ
-	public function cart()
-	{
-		$data = [];
-		$data['cat_list'] = $this->inventory_model->get_category_list();
+// モデルより、カートの情報を取得します。
+        $cart = $this->cart_model->get_all();
+        $data['total']      = $cart['total'];
+        $data['cart']       = $cart['items'];
+        $data['item_count'] = $cart['line'];
 
-# モデルより、カートの情報を取得します。
-		$cart = $this->cart_model->get_all();
-		$data['total']      = $cart['total'];
-		$data['cart']       = $cart['items'];
-		$data['item_count'] = $cart['line'];
+        $data['main'] = 'shop_cart';
+        $this->twig->display('shop_tmpl_shop', $data);
+    }
 
-		$data['main'] = 'shop_cart';
-		$this->twig->display('shop_tmpl_shop', $data);
-	}
+    // 検索ページ
+    public function search($offset = '0'): void
+    {
+// オフセットを検証します。
+        $this->load->library('validation/field_validation');
+        $this->field_validation->validate(
+            $offset,
+            'required|is_natural|max_length[3]'
+        );
 
-	// 検索ページ
-	public function search($offset = '0')
-	{
-# オフセットを検証します。
-		$this->load->library('validation/field_validation');
-		$this->field_validation->validate(
-			$offset, 'required|is_natural|max_length[3]'
-		);
+// 検索キーワードをクエリ文字列から取得します。
+        $q = (string) $this->input->get('q');
+// 全角スペースを半角スペースに変換します。
+        $q = trim(mb_convert_kana($q, 's'));
+// 検索キーワードを検証します。
+        $this->field_validation->validate(
+            $q,
+            'max_length[100]'
+        );
 
-# 検索キーワードをクエリ文字列から取得します。
-		$q = (string) $this->input->get('q');
-# 全角スペースを半角スペースに変換します。
-		$q = trim(mb_convert_kana($q, 's'));
-# 検索キーワードを検証します。
-		$this->field_validation->validate(
-			$q, 'max_length[100]'
-		);
+        $data = [];
+        $data['cat_list'] = $this->inventory_model->get_category_list();
 
-		$data = [];
-		$data['cat_list'] = $this->inventory_model->get_category_list();
+// モデルから、キーワードで検索した商品データと総件数を取得します。
+        $data['list'] = $this->inventory_model->get_product_by_search(
+            $q,
+            $this->limit,
+            $offset
+        );
+        $total = $this->inventory_model->get_count_by_search($q);
 
-# モデルから、キーワードで検索した商品データと総件数を取得します。
-		$data['list'] = $this->inventory_model->get_product_by_search(
-			$q, $this->limit, $offset
-		);
-		$total = $this->inventory_model->get_count_by_search($q);
+// ページネーションを生成します。
+        $this->load->library('generate_pagination');
+        $path  = '/shop/search';
+        $data['pagination'] = $this->generate_pagination->get_links($path, $total, 3);
 
-# ページネーションを生成します。
-		$this->load->library('generate_pagination');
-		$path  = '/shop/search';
-		$data['pagination'] = $this->generate_pagination->get_links($path, $total, 3);
+        $data['q'] = $q;
+        $data['total'] = $total;
 
-		$data['q'] = $q;
-		$data['total'] = $total;
+        $data['main']   = 'shop_search';
+        $data['item_count'] = $this->cart_model->count();
+        $this->twig->display('shop_tmpl_shop', $data);
+    }
 
-		$data['main']   = 'shop_search';
-		$data['item_count'] = $this->cart_model->count();
-		$this->twig->display('shop_tmpl_shop', $data);
-	}
+    // お客様情報入力ページ
+    public function customer_info(): void
+    {
+// 検証ルールを設定します。
+        $this->load->library('validation/shop_validation_customer');
+        $this->form_validation->run();
 
-	// お客様情報入力ページ
-	public function customer_info()
-	{
-# 検証ルールを設定します。
-		$this->load->library('validation/shop_validation_customer');
-		$this->form_validation->run();
+        $data = [
+            'action' => 'お客様情報の入力',
+            'main'   => 'shop_customer_info',
+        ];
+        $this->twig->display('shop_tmpl_checkout', $data);
+    }
 
-		$data = [
-			'action' => 'お客様情報の入力',
-			'main'   => 'shop_customer_info',
-		];
-		$this->twig->display('shop_tmpl_checkout', $data);
-	}
+    // 注文内容確認
+    public function confirm(): void
+    {
+        $this->load->library('validation/shop_validation_customer');
 
-	// 注文内容確認
-	public function confirm()
-	{
-		$this->load->library('validation/shop_validation_customer');
+        if ($this->form_validation->run() == true) {
+// 検証をパスした入力データは、モデルを使って保存します。
+            $data = [
+                'name'  => $this->input->post('name'),
+                'zip'   => $this->input->post('zip'),
+                'addr'  => $this->input->post('addr'),
+                'tel'   => $this->input->post('tel'),
+                'email' => $this->input->post('email'),
+            ];
+            $this->customer_model->set($data);
 
-		if ($this->form_validation->run() == TRUE)
-		{
-# 検証をパスした入力データは、モデルを使って保存します。
-			$data = [
-				'name'  => $this->input->post('name'),
-				'zip'   => $this->input->post('zip'),
-				'addr'  => $this->input->post('addr'),
-				'tel'   => $this->input->post('tel'),
-				'email' => $this->input->post('email'),
-			];
-			$this->customer_model->set($data);
+            $cart = $this->cart_model->get_all();
+            $data['total'] = $cart['total'];
+            $data['cart']  = $cart['items'];
 
-			$cart = $this->cart_model->get_all();
-			$data['total'] = $cart['total'];
-			$data['cart']  = $cart['items'];
+            $data['action'] = '注文内容の確認';
+            $data['main']   = 'shop_confirm';
+        } else {
+            $data = [
+                'action' => 'お客様情報の入力',
+                'main'   => 'shop_customer_info',
+            ];
+        }
 
-			$data['action'] = '注文内容の確認';
-			$data['main']   = 'shop_confirm';
-		}
-		else
-		{
-			$data = [
-				'action' => 'お客様情報の入力',
-				'main'   => 'shop_customer_info',
-			];
-		}
+        $this->twig->display('shop_tmpl_checkout', $data);
+    }
 
-		$this->twig->display('shop_tmpl_checkout', $data);
-	}
-
-	// 注文処理
-	public function order()
-	{
-		if ($this->cart_model->count() == 0)
-		{
-			echo '買い物カゴには何も入っていません。';
-		}
-# モデルのorder()メソッドを呼び出し、注文データの処理を依頼します。
-		elseif ($this->shop_model->order())
-		{
-			$data = [
-				'action' => '注文の完了',
-				'main'   => 'shop_thankyou',
-			];
-			$this->twig->display('shop_tmpl_checkout', $data);
-# 注文が完了したので、セッションを破棄します。
-			$this->session->sess_destroy();
-		}
-		else
-		{
-			echo 'システムエラー';
-		}
-	}
+    // 注文処理
+    public function order(): void
+    {
+        if ($this->cart_model->count() == 0) {
+            echo '買い物カゴには何も入っていません。';
+        }
+// モデルのorder()メソッドを呼び出し、注文データの処理を依頼します。
+        elseif ($this->shop_model->order()) {
+            $data = [
+                'action' => '注文の完了',
+                'main'   => 'shop_thankyou',
+            ];
+            $this->twig->display('shop_tmpl_checkout', $data);
+// 注文が完了したので、セッションを破棄します。
+            $this->session->sess_destroy();
+        } else {
+            echo 'システムエラー';
+        }
+    }
 }
