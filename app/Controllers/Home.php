043c81a84fce0accc8eb7b04b74dd273bf2b30d3
kenjis <kenji.uui@gmail.com>
2021-01-23 11:43:40 +0900

    docs: @property を追加

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Kenjis\CI3Compatible\Core\CI_Controller;
+use Kenjis\CI3Compatible\Core\CI_Output;
 
+/**
+ * @property CI_Output $output
+ */
 class Home extends CI_Controller
 {
     public function index(): void
