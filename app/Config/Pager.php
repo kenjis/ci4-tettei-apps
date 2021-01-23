b35769d9785840d413b445704cd5bd5e68cdbf36
kenjis <kenji.uui@gmail.com>
2021-01-23 11:46:01 +0900

    feat: ページネーションのテンプレートを追加

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -21,7 +21,7 @@ class Pager extends BaseConfig
 	 * @var array<string, string>
 	 */
 	public $templates = [
-		'default_full'   => 'CodeIgniter\Pager\Views\default_full',
+		'default_full'   => 'App\Views\Pager\default_full',
 		'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
 		'default_head'   => 'CodeIgniter\Pager\Views\default_head',
 	];
