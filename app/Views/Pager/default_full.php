85971349ade10c77c12c0dc5ea72e82d8b286b33
kenjis <kenji.uui@gmail.com>
2021-01-25 10:09:59 +0900

    fix: ページネーションリンクを修正
    «最初 と 最後» が必要ない場合は出力されないように

--- app/Views/Pager/default_full.php
+++ app/Views/Pager/default_full.php
@@ -8,25 +8,29 @@ $pager->setSurroundCount(2);
 ?>
 
 <p class="pagination">
-    <?php if ($pager->hasPreviousPage()) : ?>
+<?php if ($pager->hasPreviousPage()) : ?>
+    <?php if ($pager->current !== 2) : ?>
     <a href="<?= $pager->getFirst() ?>" data-ci-pagination-page="<?= $pager->first ?> rel="start">&laquo;<?= lang('Pager.first') ?></a>
+    <?php endif ?>
 
     <a href="<?= $pager->getPreviousPage() ?>" data-ci-pagination-page="<?= $pager->current - 1 ?>" rel="prev">&lt;</a>
-    <?php endif ?>
+<?php endif ?>
 
-    <?php foreach ($pager->links() as $link) : ?>
-    <?php if ($link['active']) : ?>
-        <strong><?= $link['title'] ?></strong>
-    <?php else : ?>
-        <a href="<?= $link['uri'] ?>" data-ci-pagination-page="<?= $link['title'] ?>">
-            <?= $link['title'] ?>
-        </a>
-    <?php endif ?>
-    <?php endforeach ?>
+<?php foreach ($pager->links() as $link) : ?>
+<?php if ($link['active']) : ?>
+    <strong><?= $link['title'] ?></strong>
+<?php else : ?>
+    <a href="<?= $link['uri'] ?>" data-ci-pagination-page="<?= $link['title'] ?>">
+        <?= $link['title'] ?>
+    </a>
+<?php endif ?>
+<?php endforeach ?>
 
-    <?php if ($pager->hasNextPage()) : ?>
+<?php if ($pager->hasNextPage()) : ?>
     <a href="<?= $pager->getNextPage() ?>" data-ci-pagination-page="<?= $pager->current + 1 ?>" rel="next">&gt;</a>
 
+    <?php if ($pager->current !== ($pager->last - 1)) : ?>
     <a href="<?= $pager->getLast() ?>" data-ci-pagination-page="<?= $pager->last ?>"><?= lang('Pager.last') ?>&raquo;</a>
     <?php endif ?>
+<?php endif ?>
 </p>
