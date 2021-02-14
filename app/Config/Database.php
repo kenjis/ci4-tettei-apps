18f016b5b61394922443fdd670b308814d050b1a
kenjis <kenji.uui@gmail.com>
2021-02-14 11:14:21 +0900

    config: テスト用データベースの設定を追加
    .env の設定が何故か使われないため

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -58,12 +58,12 @@ class Database extends Config
 	 */
 	public $tests = [
 		'DSN'      => '',
-		'hostname' => '127.0.0.1',
-		'username' => '',
-		'password' => '',
-		'database' => ':memory:',
-		'DBDriver' => 'SQLite3',
-		'DBPrefix' => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+		'hostname' => 'localhost',
+		'username' => 'username',
+		'password' => 'password',
+		'database' => 'codeigniter',
+		'DBDriver' => 'MySQLi',
+		'DBPrefix' => '',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
 		'pConnect' => false,
 		'DBDebug'  => (ENVIRONMENT !== 'production'),
 		'charset'  => 'utf8',
