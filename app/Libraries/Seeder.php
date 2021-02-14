a63c5c5a6cf12c148f04317399538f00543549c5
kenjis <kenji.uui@gmail.com>
2021-02-14 09:16:31 +0900

    style: composer cs-fix

--- app/Libraries/Seeder.php
+++ app/Libraries/Seeder.php
@@ -1,43 +1,37 @@
 <?php
-/**
- * 『CodeIgniter徹底入門』のサンプルアプリケーションをCodeIgniter 3.0にアップデート
- *
- * @author     Kenji Suzuki <https://github.com/kenjis>
- * @license    BSD 3-Clause License
- * @copyright  2015 Kenji Suzuki
- * @link       https://github.com/kenjis/codeigniter-tettei-apps
- */
+
+declare(strict_types=1);
 
 class Seeder
 {
-	private $CI;
-	protected $db;
-	protected $dbforge;
+    private $CI;
+    protected $db;
+    protected $dbforge;
 
-	public function __construct()
-	{
-		$this->CI =& get_instance();
-		$this->CI->load->database();
-		$this->CI->load->dbforge();
-		$this->db = $this->CI->db;
-		$this->dbforge = $this->CI->dbforge;
-	}
+    public function __construct()
+    {
+        $this->CI =& get_instance();
+        $this->CI->load->database();
+        $this->CI->load->dbforge();
+        $this->db = $this->CI->db;
+        $this->dbforge = $this->CI->dbforge;
+    }
 
-	/**
-	 * Run another seeder
-	 * 
-	 * @param string $seeder Seeder classname
-	 */
-	public function call($seeder)
-	{
-		$file = APPPATH . 'database/seeds/' . $seeder . '.php';
-		require_once $file;
-		$obj = new $seeder;
-		$obj->run();
-	}
+    /**
+     * Run another seeder
+     *
+     * @param string $seeder Seeder classname
+     */
+    public function call(string $seeder): void
+    {
+        $file = APPPATH . 'database/seeds/' . $seeder . '.php';
+        require_once $file;
+        $obj = new $seeder();
+        $obj->run();
+    }
 
-	public function __get($property)
-	{
-		return $this->CI->$property;
-	}
+    public function __get($property)
+    {
+        return $this->CI->$property;
+    }
 }
