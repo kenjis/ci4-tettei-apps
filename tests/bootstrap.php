5dadf8ae8975c8deef08a0b3c06c24b26fdd81a7
kenjis <kenji.uui@gmail.com>
2021-02-14 11:11:19 +0900

    config: テスト用のbootstrapを追加

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Kenjis\CI3Compatible\Core\CI_Controller;
+
+require __DIR__ . '/../vendor/codeigniter4/codeigniter4/system/Test/bootstrap.php';
+
+new CI_Controller();
