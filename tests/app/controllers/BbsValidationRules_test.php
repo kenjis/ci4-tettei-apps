537f57f37ee9c8c2cb6f97195cbeb065d470119b
kenjis <kenji.uui@gmail.com>
2021-02-16 19:12:12 +0900

    test: BbsValidationRulesのテストを追加

---
+++ tests/app/controllers/BbsValidationRules_test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Kenjis\CI3Compatible\Test\TestCase\UnitTestCase;
+
+class BbsValidationRules_test extends UnitTestCase
+{
+    public function test_captcha_check_failure(): void
+    {
+        $controller = $this->newController(Bbs::class);
+        $controller->load->database();
+
+        $validationRule = new BbsValidationRules();
+
+        $error = '';
+        $actual = $validationRule->captcha_check('bad_input', $error);
+
+        $this->assertFalse($actual);
+
+        $expected = '画像認証コードが一致しません。';
+        $this->assertSame($expected, $error);
+    }
+}
