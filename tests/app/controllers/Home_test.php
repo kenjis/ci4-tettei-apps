75da62c1fcbbe2fc52d46fcdb1d21e698b357763
kenjis <kenji.uui@gmail.com>
2021-02-16 20:08:18 +0900

    test: TestCaseファイル名をコントローラ名に合わせる

--- tests/app/controllers/Home_test.php
+++ tests/app/controllers/Home_test.php
@@ -6,7 +6,7 @@ namespace App\Controllers;
 
 use Kenjis\CI3Compatible\Test\TestCase\FeatureTestCase;
 
-class Welcome_test extends FeatureTestCase
+class Home_test extends FeatureTestCase
 {
     public function test_index(): void
     {
