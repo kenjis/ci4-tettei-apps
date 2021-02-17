ec0e0b886d4c6be8b0ddb0218baa799e877ee4cb
kenjis <kenji.uui@gmail.com>
2021-02-17 11:57:21 +0900

    refactor: 不要なコードを削除

--- app/Controllers/MY_Controller.php
+++ app/Controllers/MY_Controller.php
@@ -15,9 +15,5 @@ class MY_Controller extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-
-        if (ENVIRONMENT !== 'development') {
-            return;
-        }
     }
 }
