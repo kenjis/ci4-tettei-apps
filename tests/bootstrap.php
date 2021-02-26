14964b866c9d69cdf2bcd553c34a3bcbbb74bab0
kenjis <kenji.uui@gmail.com>
2021-02-26 09:05:42 +0900

    chore: ci3-to-4-upgrade-helperのtest bootstrapを使用するように変更

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Kenjis\CI3Compatible\Core\CI_Controller;
-
-require __DIR__ . '/../vendor/codeigniter4/codeigniter4/system/Test/bootstrap.php';
-
-new CI_Controller();
