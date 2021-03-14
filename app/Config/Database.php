8815b0bb25209a86644cd62f52562cf5daec8fa3
kenjis <kenji.uui@gmail.com>
2021-03-14 16:54:59 +0900

    style: インデントを修正

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -54,8 +54,8 @@ class Database extends Config
 	 * This database connection is used when
 	 * running PHPUnit database tests.
 	 *
-     * phpunit実行時の設定は、tests/_support/Config/Registrar.php で上書き
-     *
+	 * phpunit実行時の設定は、tests/_support/Config/Registrar.php で上書き
+	 *
 	 * @var array
 	 */
 	public $tests = [
