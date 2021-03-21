ece0352f921f4ae7365889c96257803bcd6fcf63
kenjis <kenji.uui@gmail.com>
2021-03-21 13:53:52 +0900

    test: テストケースを追加

--- tests/app/libraries/validation/FormValidationTest.php
+++ tests/app/libraries/validation/FormValidationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Libraries\Validation;
 
+use App\Libraries\FormData;
 use App\Models\Form\FormForm;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\URI;
@@ -37,7 +38,7 @@ class FormValidationTest extends UnitTestCase
         $this->assertFalse($validated);
     }
 
-    public function test_検証が成功するとtrueが返る(): void
+    public function test_検証が成功するとtrueが返る()
     {
         $_POST = [
             'name' => '名前です',
@@ -46,11 +47,24 @@ class FormValidationTest extends UnitTestCase
         ];
         $request = new IncomingRequest(new App(), new URI(), null, new UserAgent());
 
+        $form = new FormForm();
         $validated = $this->validation->validate(
             $request,
-            new FormForm()
+            $form
         );
 
         $this->assertTrue($validated);
+
+        return $form;
+    }
+
+    /**
+     * @depends test_検証が成功するとtrueが返る
+     */
+    public function test_検証が成功するとFormDataに値がセットされる(FormData $form): void
+    {
+        $this->assertSame('名前です', $form['name']);
+        $this->assertSame('foo@example.com', $form['email']);
+        $this->assertSame('コメントです', $form['comment']);
     }
 }
